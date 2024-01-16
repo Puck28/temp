209b74ed0bcae500f866b92dad331e90a575bcc8
puck28 <justinnickel28@gmail.com>
2024-01-15 16:45:18 -0800

    update

--- src/store/api/userApi.ts
+++ src/store/api/userApi.ts
@@ -4,9 +4,8 @@ export const userApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         createUser: builder.mutation({
             query: (integer) => ({
-                url: '/Admin/create',
-                method: 'POST',
-                body: integer  
+                url: `/Admin/create?value=${integer}`,
+                method: 'Get'  
             }),
         })
     }),
